d21e4db35c2bbd984da4dc6886a091007fab5b34
Siphesihle-Thomo <seansthomo@gmail.com>
2024-09-23 22:08:16 +0200

    fix: use BufferedReader to get input from connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
@@ -17,14 +19,13 @@ public class Main {
             connection.setRequestMethod("GET");
             connection.setRequestProperty("User-Agent", USER_AGENT);
             int responseCode = connection.getResponseCode();
-            System.out.println("URL: " + GET_URL);
             System.out.println("Response Code: " + responseCode);
             if (responseCode == HttpURLConnection.HTTP_OK) {
-                Scanner input = new Scanner(connection.getInputStream());
+                BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String inputLine;
                 StringBuilder response = new StringBuilder();
 
-                while ((inputLine = input.next()) != null) {
+                while ((inputLine = input.readLine()) != null) {
                     response.append(inputLine);
                 }
 
